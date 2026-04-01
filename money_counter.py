bf8e4ff4e5922fd9a55b1a5a5a15e0b853cfff32
Mylena Lam <mylenalam101@gmail.com>
2026-03-31 22:35:30 -0500

    save

--- money_counter.py
+++ money_counter.py
@@ -1,11 +1,10 @@
 print("Enter the number of quarters, dimes, nickels, and pennies.")
 
-Quarters = int(input("Enter the number of quarters "))
-Dimes = int(input("Enter the number of dimes"))
-Nickels = int(input("Enter the number of nickels"))
-Pennies = int(input("Enter the numner of pennies"))
+Quarters = int(input("Enter the number of quarters: "))
+Dimes = int(input("Enter the number of dimes: "))
+Nickels = int(input("Enter the number of nickels: "))
+Pennies = int(input("Enter the numner of pennies: "))
 
-quarter = 25
-dime = 10
-nickel = 5
-penny = 1
+total = (quarters * .25) + (dimes * .10) + (nickels * .05) + (pennies * .01)
+
+print("You have: " total)
