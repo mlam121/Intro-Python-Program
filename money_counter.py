d3c358dcc11c4a3f0f540f746df725eeeeff8691
Mylena Lam <mylenalam101@gmail.com>
2026-03-31 22:38:30 -0500

    finish

--- money_counter.py
+++ money_counter.py
@@ -5,6 +5,13 @@ Dimes = int(input("Enter the number of dimes: "))
 Nickels = int(input("Enter the number of nickels: "))
 Pennies = int(input("Enter the numner of pennies: "))
 
-total = (quarters * .25) + (dimes * .10) + (nickels * .05) + (pennies * .01)
+total = (Quarters * .25) + (Dimes * .10) + (Nickels * .05) + (Pennies * .01)
 
-print("You have: " total)
+# Round total
+total = round(total, 2)
+
+# Format as a string with exactly 2 decimal places
+total_str = "{:.2f}".format(total)
+
+# Print
+print("You have: $" + total_str)
