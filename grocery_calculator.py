8dcd67baeb1fe30f0a79680a0d48ecf842a99cfc
mylena <mylenalam101@gmail.com>
2026-04-02 13:34:11 -0500

    added caps

--- grocery_calculator.py
+++ grocery_calculator.py
@@ -1,9 +1,9 @@
-qty_item1 = int(input("Enter quantity of item 1: "))
-price_item1 = float(input("Enter price of item 1: "))
-qty_item2 = int(input("Enter quantity of item 2: "))
-price_item2 = float(input("Enter price of item 2: "))
+qty_Item1 = int(input("Enter quantity of item 1: "))
+price_Item1 = float(input("Enter price of item 1: "))
+qty_Item2 = int(input("Enter quantity of item 2: "))
+price_Item2 = float(input("Enter price of item 2: "))
 
-subtotal = qty_item1 * price_item1 + qty_item2 * price_item2
+subtotal = qty_Item1 * price_Item1 + qty_Item2 * price_Item2
 tax = subtotal * 0.07
 grand_total = subtotal + tax
 
