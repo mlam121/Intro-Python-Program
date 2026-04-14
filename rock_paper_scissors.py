45860d04db9970bf68f701a78d81ceea8db141bd
mylena <mylenalam101@gmail.com>
2026-04-14 13:27:44 -0500

    working on

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,17 @@
 import random
 
 test_mode = input("Do you want to run in test mode? (yes/no): ")
-user_choice = input("Enter your choice (rock, paper, scissors): ")
+user_choice = input(["rock", "paper", "scissors"])
 computer_choice = random.choice(["rock", "paper", "scissors"])
+
+def test_mode():
+    print("Running in test mode...")
+    user_choice = "rock"
+    computer_choice = "scissors"
+    print(f"User choice: {user_choice}")
+    print(f"Computer choice: {computer_choice}")
+    determine_winner(user_choice, computer_choice)
+
+
+
+
