8ffdb8563f51fbc1b6d4929fa7039f41862ebb51
Mylena Lam <mylenalam101@gmail.com>
2026-04-14 17:21:51 -0500

    fix

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,17 +1,38 @@
 import random
 
-test_mode = input("Do you want to run in test mode? (yes/no): ")
-user_choice = input(["rock", "paper", "scissors"])
-computer_choice = random.choice(["rock", "paper", "scissors"])
-
-def test_mode():
-    print("Running in test mode...")
-    user_choice = "rock"
-    computer_choice = "scissors"
-    print(f"User choice: {user_choice}")
-    print(f"Computer choice: {computer_choice}")
-    determine_winner(user_choice, computer_choice)
+def get_computer_choice(test_mode=False):
+    if test_mode:
+        return "rock"
+    return random.choice(["rock", "paper", "scissors"])
 
 
+def determine_winner(user, computer):
+    if user == computer:
+        return "It's a tie!"
+    
+    if (user == "rock" and computer == "scissors") or \
+       (user == "paper" and computer == "rock") or \
+       (user == "scissors" and computer == "paper"):
+        return "You win!"
+    
+    return "Computer wins!"
 
 
+def play_game():
+    # Ask if test mode should be on
+    test_input = input("Enable test mode? (yes/no): ").lower()
+    test_mode = test_input == "yes"
+
+    user_choice = input("Enter rock, paper, or scissors: ").lower()
+    
+    if user_choice not in ["rock", "paper", "scissors"]:
+        print("Invalid choice!")
+        return
+
+    computer_choice = get_computer_choice(test_mode)
+
+    print(f"Computer chose: {computer_choice}")
+    print(determine_winner(user_choice, computer_choice))
+
+
+play_game()
