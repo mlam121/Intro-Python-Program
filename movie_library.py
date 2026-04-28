4920934be3ec3707b57824fe8090b386107f1382
Mylena Lam <mylenalam101@gmail.com>
2026-04-28 13:56:46 -0500

    space

--- movie_library.py
+++ movie_library.py
@@ -35,7 +35,7 @@ def sort_movies():
    if not sorted_movies:
        print("No movies in the library.")
    else:
-       print("\nTitle                         Year    Genre           Rating")
+       print("\nTitle             Year    Genre           Rating")
        print("--------------------------------------")
        for title, details in sorted_movies.items():
            print(f"{title:<18} {details[0]:<6} {details[1]:<12} {details[2]}")
