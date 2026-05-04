285723d35e6ce69c59b983ff6a98ef55f62be9fe
mylena <mylenalam101@gmail.com>
2026-05-04 11:20:35 -0500

    working

--- conversion.py
+++ conversion.py
@@ -1,5 +1,6 @@
 """Functions that convert metric and imperial units."""
 
+# Temperature 
 def cel2fah(c):
     """Converts from Celsius to Fahrenheit."""
     return 9/5 * c + 32
@@ -8,6 +9,7 @@ def fah2cel(f):
     """Converts from Fahrenheit to Celsius."""
     return 5/9 * (f - 32)
 
+# Length 
 def km2mi(km):
     """Converts from kilometers to miles."""
     return km / 1.60934
@@ -15,14 +17,6 @@ def km2mi(km):
 def mi2km(mi):
     """Converts from miles to kilometers."""
     return mi * 1.60934
-
-def kg2lb(kg):
-    """Converts from kilograms to pounds."""
-    return kg * 2.20462
-
-def lb2kg(lb):
-    """Converts from pounds to kilograms."""
-    return lb / 2.20462
 
 def cm2in(cm):
     """Converts from centimeters to inches."""
@@ -32,6 +26,16 @@ def in2cm(inches):
     """Converts from inches to centimeters."""
     return inches * 2.54
 
+# Weight 
+def kg2lb(kg):
+    """Converts from kilograms to pounds."""
+    return kg * 2.20462
+
+def lb2kg(lb):
+    """Converts from pounds to kilograms."""
+    return lb / 2.20462
+
+# Volume 
 def l2gal(liters):
     """Converts from liters to gallons."""
     return liters / 3.78541
